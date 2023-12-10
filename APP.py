b614a0e5cf7cafbeeeeb6bc14244facd1a28e9d6
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-12-10 15:51:40 -0600

    Create APP.py

---
+++ APP.py
@@ -0,0 +1,39 @@
+
+import streamlit as st
+import PIL
+
+st.title("Detección de Plagas en la agricultura Mexicana")
+st.write ("APLICACION PARA LA DETECCIÓN DE INSECTOS E ACAROS EN LA AGRICULTURA MEXICANA ")
+
+# x = st.slider ("Selecciona un valor ")
+# st.write (x, " al cuadrado es ", x*x)
+
+
+# Barra lateral
+st.sidebar.header("Configuración del modelo de aprendizaje automático")
+
+
+genres = st.sidebar.multiselect(
+    "Seleccione las modelos Detección",
+    ["YoloV8",  "ResNet50"],
+    default=None,
+)
+
+
+st.sidebar.header("Buscar Imagen")
+
+
+Carimagen = None
+Carimagen = st.sidebar.file_uploader(
+        "Elige una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+
+if Carimagen:
+   uploaded_image = PIL.Image.open(Carimagen)
+   st.image(Carimagen, caption="Imagen Cargada",
+        use_column_width=True)
+
+
+
+
+st.sidebar.button('DETECTAR PLAGA')
+
