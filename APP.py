747d47ea6f49a2cdb0ebbba063116e0360ca3c76
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-12-11 08:05:58 -0600

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,93 +1,38 @@
-# Python In-built packages
-from pathlib import Path
+import streamlit as st
 import PIL
 
-# External packages
-import streamlit as st
+st.title("Detección de Plagas en la agricultura Mexicana")
+st.write ("APLICACION PARA LA DETECCIÓN DE INSECTOS E ACAROS EN LA AGRICULTURA MEXICANA ")
 
-# Local Modules
-import settings
-import helper
+# x = st.slider ("Selecciona un valor ")
+# st.write (x, " al cuadrado es ", x*x)
 
-# Setting page layout
-st.set_page_config(
-    page_title="Deteccion de Plagas en la agricultura Mexicana",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
 
-# Main page heading
-
-# Sidebar
+# Barra lateral
 st.sidebar.header("Configuración del modelo de aprendizaje automático")
 
-# Model Options
+
 genres = st.sidebar.multiselect(
     "Seleccione las modelos Detección",
-    ["YoloV8", "ResNet50"],
+    ["YoloV8",  "ResNet50"],
     default=None,
 )
 
-confidence = float(st.sidebar.slider(
-    "Seleccione la confianza del modelo", 25, 100, 40)) / 100
 
-# Selecting Detection Or Segmentation
-if "YoloV8" in genres:
-    model_path = Path(settings.DETECTION_MODEL)
-#elif "ResNet50" in genres:
-#  model_path = Path(settings.RESNET50_MODEL)
-else:
-    st.error("Seleccione al menos un modelo.")
+st.sidebar.header("Buscar Imagen")
 
-# Load Pre-trained ML Model
-try:
-    model = helper.load_model(model_path)
-except Exception as ex:
-    st.error(f"Unable to load model. Check the specified path: {model_path}")
-    st.error(ex)
 
-st.sidebar.header("Imagen/Config")
-source_radio = st.sidebar.radio(
-    "Seleccione Fuente", settings.SOURCES_LIST)
-
-source_img = None
-# If image is selected
-if source_radio == settings.IMAGE:
-    source_img = st.sidebar.file_uploader(
+Carimagen = None
+Carimagen = st.sidebar.file_uploader(
         "Elige una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
 
-    col1, col2 = st.columns(2)
+if Carimagen:
+   uploaded_image = PIL.Image.open(Carimagen)
+   st.image(Carimagen, caption="Imagen Cargada",
+        use_column_width=True)
 
-    with col1:
-        try:
-            if source_img:
-                uploaded_image = PIL.Image.open(source_img)
-                st.image(source_img, caption="Imagen Cargada",
-                         use_column_width=True)
-        except Exception as ex:
-            st.error("Se produjo un error al abrir la imagen.")
-            st.error(ex)
 
-    with col2:
-        if st.sidebar.button('Detectar Objeto'):
-            res = model.predict(uploaded_image,
-                                conf=confidence
-                                )
-            boxes = res[0].boxes
-            res_plotted = res[0].plot()[:, :, ::-1]
-            st.image(res_plotted, caption='Detected Image',
-                     use_column_width=True)
-            try:
-                with st.expander("Resultados de la detección"):
-                    for box in boxes:
-                        st.write(box.data)
-            except Exception as ex:
-                # st.write(ex)
-                st.write("No image is uploaded yet!")
 
-elif source_radio == settings.WEBCAM:
-    helper.play_webcam(confidence, model)
 
-else:
-    st.error("Please select a valid source type!")
+st.sidebar.button('DETECTAR PLAGA')
 
