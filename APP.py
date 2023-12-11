9a49f41ad148dc9a7d3b411fa6f1ddaaa7edfb91
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-12-10 21:00:54 -0600

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,6 +1,12 @@
-
-import streamlit as st
+from pathlib import Path
 import PIL
+# 
+import streamlit as st
+
+# Local Modules
+import settings
+import helper
+
 
 st.title("Detección de Plagas en la agricultura Mexicana")
 st.write ("APLICACION PARA LA DETECCIÓN DE INSECTOS E ACAROS EN LA AGRICULTURA MEXICANA ")
@@ -19,6 +25,16 @@ genres = st.sidebar.multiselect(
     default=None,
 )
 
+# Selecting Detection Or Segmentation
+if model_type == 'YoloV8':
+    model_path = Path(settings.DETECTION_MODEL)
+
+# Load Pre-trained ML Model
+try:
+    model = helper.load_model(model_path)
+except Exception as ex:
+    st.error(f"Unable to load model. Check the specified path: {model_path}")
+    st.error(ex)
 
 st.sidebar.header("Buscar Imagen")
 
@@ -27,13 +43,34 @@ Carimagen = None
 Carimagen = st.sidebar.file_uploader(
         "Elige una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
 
-if Carimagen:
-   uploaded_image = PIL.Image.open(Carimagen)
-   st.image(Carimagen, caption="Imagen Cargada",
-        use_column_width=True)
+col1, col2 = st.columns(2)
+
+
+with col1:
+        try:
+            if Carimagen:
+                uploaded_image = PIL.Image.open(Carimagen)
+                st.image(Carimagen, caption="Imagen Cargada",use_column_width=True)
+
+with col2:        
+            if st.sidebar.button('DETECTAR PLAGA'):
+                res = model.predict(uploaded_image,
+                                    conf=confidence
+                                    )
+                boxes = res[0].boxes
+                res_plotted = res[0].plot()[:, :, ::-1]
+                st.image(res_plotted, caption='Detected Image',
+                         use_column_width=True)
+                try:
+                    with st.expander("Resultados de la detección"):
+                        for box in boxes:
+                            st.write(box.data)
+                except Exception as ex:
+                    # st.write(ex)
+                    st.write("No image is uploaded yet!")
 
 
 
 
-st.sidebar.button('DETECTAR PLAGA')
+# st.sidebar.button('DETECTAR PLAGA')
 
