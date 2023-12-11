dc85e4a717f47337691837e80b0425ccb5a9e9f9
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-12-10 22:07:49 -0600

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,31 +1,28 @@
 from pathlib import Path
 import PIL
-# 
 import streamlit as st
 
 # Local Modules
 import settings
 import helper
 
-
 st.title("Detección de Plagas en la agricultura Mexicana")
-st.write ("APLICACION PARA LA DETECCIÓN DE INSECTOS E ACAROS EN LA AGRICULTURA MEXICANA ")
+st.write("APLICACION PARA LA DETECCIÓN DE INSECTOS E ACAROS EN LA AGRICULTURA MEXICANA ")
 
 # x = st.slider ("Selecciona un valor ")
 # st.write (x, " al cuadrado es ", x*x)
 
-
 # Barra lateral
 st.sidebar.header("Configuración del modelo de aprendizaje automático")
 
-
 genres = st.sidebar.multiselect(
     "Seleccione las modelos Detección",
-    ["YoloV8",  "ResNet50"],
+    ["YoloV8", "ResNet50"],
     default=None,
 )
 
 # Selecting Detection Or Segmentation
+model_type = genres[0]  # Selecciona el primer elemento de la lista
 if model_type == 'YoloV8':
     model_path = Path(settings.DETECTION_MODEL)
 
@@ -38,33 +35,30 @@ except Exception as ex:
 
 st.sidebar.header("Buscar Imagen")
 
-
 Carimagen = None
-Carimagen = st.sidebar.file_uploader(
-        "Elige una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+Carimagen = st.sidebar.file_uploader("Elige una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
 
 col1, col2 = st.columns(2)
 
+with col1:
+    try:
+        if Carimagen:
+            uploaded_image = PIL.Image.open(Carimagen)
+            st.image(Carimagen, caption="Imagen Cargada", use_column_width=True)
+    except Exception as ex:
+        st.error(ex)
 
-  with col1:
+with col2:
+    if st.sidebar.button('DETECTAR PLAGA'):
+        res = model.predict(uploaded_image, conf=confidence)
+        boxes = res[0].boxes
+        res_plotted = res[0].plot()[:, :, ::-1]
+        st.image(res_plotted, caption='Detected Image', use_column_width=True)
         try:
-            if Carimagen:
-                uploaded_image = PIL.Image.open(Carimagen)
-                st.image(Carimagen, caption="Imagen Cargada",use_column_width=True)
+            with st.expander("Resultados de la detección"):
+                for box in boxes:
+                    st.write(box.data)
+        except Exception as ex:
+            st.write("¡Aún no se ha subido ninguna imagen!")
 
-   with col2:        
-            if st.sidebar.button('DETECTAR PLAGA'):
-                res = model.predict(uploaded_image,
-                                    conf=confidence
-                                    )
-                boxes = res[0].boxes
-                res_plotted = res[0].plot()[:, :, ::-1]
-                st.image(res_plotted, caption='Detected Image',
-                         use_column_width=True)
-                try:
-                    with st.expander("Resultados de la detección"):
-                        for box in boxes:
-                            st.write(box.data)
-                except Exception as ex:
-                    # st.write(ex)
                     st.write("¡Aún no se ha subido ninguna imagen!")
