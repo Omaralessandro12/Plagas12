274efa3f311b9ed6946f8f0f545fb0321ed49da6
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-12-10 22:14:57 -0600

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,30 +1,43 @@
+# Python In-built packages
 from pathlib import Path
 import PIL
+
+# External packages
 import streamlit as st
 
 # Local Modules
 import settings
 import helper
 
-st.title("Detección de Plagas en la agricultura Mexicana")
-st.write("APLICACION PARA LA DETECCIÓN DE INSECTOS E ACAROS EN LA AGRICULTURA MEXICANA ")
+# Setting page layout
+st.set_page_config(
+    page_title="Deteccion de Plagas en la agricultura Mexicana",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
-# x = st.slider ("Selecciona un valor ")
-# st.write (x, " al cuadrado es ", x*x)
+# Main page heading
 
-# Barra lateral
+# Sidebar
 st.sidebar.header("Configuración del modelo de aprendizaje automático")
 
+# Model Options
 genres = st.sidebar.multiselect(
     "Seleccione las modelos Detección",
     ["YoloV8", "ResNet50"],
     default=None,
 )
 
+confidence = float(st.sidebar.slider(
+    "Seleccione la confianza del modelo", 25, 100, 40)) / 100
+
 # Selecting Detection Or Segmentation
-model_type = genres[0]  # Selecciona el primer elemento de la lista
-if model_type == 'YoloV8':
-    model_path = Path(settings.DETECTION_MODEL)
+if "YoloV8" in genres:
+    model_path = Path(settings.YOLOV8_MODEL)
+elif "ResNet50" in genres:
+    model_path = Path(settings.RESNET50_MODEL)
+else:
+    st.error("Seleccione al menos un modelo.")
 
 # Load Pre-trained ML Model
 try:
@@ -33,30 +46,48 @@ except Exception as ex:
     st.error(f"Unable to load model. Check the specified path: {model_path}")
     st.error(ex)
 
-st.sidebar.header("Buscar Imagen")
+st.sidebar.header("Imagen/Config")
+source_radio = st.sidebar.radio(
+    "Seleccione Fuente", settings.SOURCES_LIST)
 
-Carimagen = None
-Carimagen = st.sidebar.file_uploader("Elige una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+source_img = None
+# If image is selected
+if source_radio == settings.IMAGE:
+    source_img = st.sidebar.file_uploader(
+        "Elige una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
 
-col1, col2 = st.columns(2)
+    col1, col2 = st.columns(2)
 
-with col1:
-    try:
-        if Carimagen:
-            uploaded_image = PIL.Image.open(Carimagen)
-            st.image(Carimagen, caption="Imagen Cargada", use_column_width=True)
-    except Exception as ex:
-        st.error(ex)
-
-with col2:
-    if st.sidebar.button('DETECTAR PLAGA'):
-        res = model.predict(uploaded_image, conf=confidence)
-        boxes = res[0].boxes
-        res_plotted = res[0].plot()[:, :, ::-1]
-        st.image(res_plotted, caption='Detected Image', use_column_width=True)
+    with col1:
         try:
-            with st.expander("Resultados de la detección"):
-                for box in boxes:
-                    st.write(box.data)
+            if source_img:
+                uploaded_image = PIL.Image.open(source_img)
+                st.image(source_img, caption="Imagen Cargada",
+                         use_column_width=True)
         except Exception as ex:
-            st.write("¡Aún no se ha subido ninguna imagen!")
+            st.error("Se produjo un error al abrir la imagen.")
+            st.error(ex)
+
+    with col2:
+        if st.sidebar.button('Detectar Objeto'):
+            res = model.predict(uploaded_image,
+                                conf=confidence
+                                )
+            boxes = res[0].boxes
+            res_plotted = res[0].plot()[:, :, ::-1]
+            st.image(res_plotted, caption='Detected Image',
+                     use_column_width=True)
+            try:
+                with st.expander("Resultados de la detección"):
+                    for box in boxes:
+                        st.write(box.data)
+            except Exception as ex:
+                # st.write(ex)
+                st.write("No image is uploaded yet!")
+
+elif source_radio == settings.WEBCAM:
+    helper.play_webcam(confidence, model)
+
+else:
+    st.error("Please select a valid source type!")
+
