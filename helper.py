e08346c099adc3131ab64036e39b16251a9f7700
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-12-13 08:21:17 -0600

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,108 +1,255 @@
-# Python In-built packages
-from pathlib import Path
-import PIL
-
-# External packages
+from ultralytics import YOLO
 import streamlit as st
+from streamlit_webrtc import WebRtcMode, webrtc_streamer, VideoTransformerBase
+from turn import get_ice_servers
 
-# Local Modules
+import numpy as np
+from PIL import Image
+import av
+import cv2
+from pytube import YouTube
+
 import settings
-import helper
-
-# Setting page layout
-st.set_page_config(
-    page_title="Deteccion de Plagas en la agricultura Mexicana",
-    # page_icon="🤖",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
 
 
-# Sidebar
-st.sidebar.header("Configuración del modelo de aprendizaje automático")
+def load_model(model_path):
+    """
+    Loads a YOLO object detection model from the specified model_path.
 
-# Model Options
-model_types_available = ['Deteccion', 'OtraTarea', 'OtraTarea2']  # Agrega más tareas según sea necesario
-model_type = st.sidebar.multiselect("Seleccionar tarea", model_types_available, default=['Deteccion'])
+    Parameters:
+        model_path (str): The path to the YOLO model file.
 
-
-
-#model_type = st.sidebar.radio(
-#    "Seleccionar tarea", ['Deteccion' ])
-
-
-confidence = float(st.sidebar.slider(
-    "Seleccione la confianza del modelo", 25, 100, 40)) / 100
+    Returns:
+        A YOLO object detection model.
+    """
+    model = YOLO(model_path)
+    return model
 
 
-if not model_type:
-    model_type = ['Deteccion']
+def display_tracker_options():
+    display_tracker = st.radio("Display Tracker", ('Yes', 'No'))
+    is_display_tracker = True if display_tracker == 'Yes' else False
+    if is_display_tracker:
+        tracker_type = st.radio("Tracker", ("bytetrack.yaml", "botsort.yaml"))
+        return is_display_tracker, tracker_type
+    return is_display_tracker, None
 
-selected_task = model_type[0]
 
-if selected_task == 'Deteccion':
-    model_path = Path(settings.DETECTION_MODEL)
+def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=None, tracker=None):
+    """
+    Display the detected objects on a video frame using the YOLOv8 model.
 
-# Selecting Detection Or Segmentation
-#if model_type == 'Deteccion':
-#   model_path = Path(settings.DETECTION_MODEL)
-#elif model_type == 'Segmentation':
-#    model_path = Path(settings.SEGMENTATION_MODEL)
+    Args:
+    - conf (float): Confidence threshold for object detection.
+    - model (YoloV8): A YOLOv8 object detection model.
+    - st_frame (Streamlit object): A Streamlit object to display the detected video.
+    - image (numpy array): A numpy array representing the video frame.
+    - is_display_tracking (bool): A flag indicating whether to display object tracking (default=None).
 
-# Load Pre-trained ML Model
-try:
-    model = helper.load_model(model_path)
-except Exception as ex:
-    st.error(f"Unable to load model. Check the specified path: {model_path}")
-    st.error(ex)
+    Returns:
+    None
+    """
 
-st.sidebar.header("Imagen/Config")
-source_radio = st.sidebar.radio(
-    "Seleccione Fuente", settings.SOURCES_LIST)
+    # Resize the image to a standard size
+    image = cv2.resize(image, (720, int(720*(9/16))))
 
-source_img = None
-# If image is selected
-if source_radio == settings.IMAGE:
-    source_img = st.sidebar.file_uploader(
-        "Elige una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+    # Display object tracking, if specified
+    if is_display_tracking:
+        res = model.track(image, conf=conf, persist=True, tracker=tracker)
+    else:
+        # Predict the objects in the image using the YOLOv8 model
+        res = model.predict(image, conf=conf)
 
-    col1, col2 = st.columns(2)
+    # # Plot the detected objects on the video frame
+    res_plotted = res[0].plot()
+    st_frame.image(res_plotted,
+                   caption='Detected Video',
+                   channels="BGR",
+                   use_column_width=True
+                   )
 
-    with col1:
+
+def play_youtube_video(conf, model):
+    """
+    Plays a webcam stream. Detects Objects in real-time using the YOLOv8 object detection model.
+
+    Parameters:
+        conf: Confidence of YOLOv8 model.
+        model: An instance of the `YOLOv8` class containing the YOLOv8 model.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    source_youtube = st.sidebar.text_input("YouTube Video url")
+
+    is_display_tracker, tracker = display_tracker_options()
+
+    if st.sidebar.button('Detect Objects'):
         try:
-            if source_img:
-                uploaded_image = PIL.Image.open(source_img)
-                st.image(source_img, caption="Imagen Cargada",
-                         use_column_width=True)
-        except Exception as ex:
-            st.error("Se produjo un error al abrir la imagen.")
-            st.error(ex)
+            yt = YouTube(source_youtube)
+            stream = yt.streams.filter(file_extension="mp4", res=720).first()
+            vid_cap = cv2.VideoCapture(stream.url)
 
-    with col2:        
-            if st.sidebar.button('Detectar Objeto'):
-                res = model.predict(uploaded_image,
-                                    conf=confidence
-                                    )
-                boxes = res[0].boxes
-                res_plotted = res[0].plot()[:, :, ::-1]
-                st.image(res_plotted, caption='Detected Image',
-                         use_column_width=True)
-                try:
-                    with st.expander("Resultados de la detección"):
-                        for box in boxes:
-                            st.write(box.data)
-                except Exception as ex:
-                    # st.write(ex)
-                    st.write("No image is uploaded yet!")
+            st_frame = st.empty()
+            while (vid_cap.isOpened()):
+                success, image = vid_cap.read()
+                if success:
+                    _display_detected_frames(conf,
+                                             model,
+                                             st_frame,
+                                             image,
+                                             is_display_tracker,
+                                             tracker
+                                             )
+                else:
+                    vid_cap.release()
+                    break
+        except Exception as e:
+            st.sidebar.error("Error loading video: " + str(e))
 
-#elif source_radio == settings.VIDEO:
-#    helper.play_stored_video(confidence, model)
 
-elif source_radio == settings.WEBCAM:
-    helper.play_webcam(confidence, model)
+def play_rtsp_stream(conf, model):
+    """
+    Plays an rtsp stream. Detects Objects in real-time using the YOLOv8 object detection model.
 
-#elif source_radio == settings.YOUTUBE:
-#   helper.play_youtube_video(confidence, model)
+    Parameters:
+        conf: Confidence of YOLOv8 model.
+        model: An instance of the `YOLOv8` class containing the YOLOv8 model.
 
-else:
-    st.error("Please select a valid source type!")
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    source_rtsp = st.sidebar.text_input("rtsp stream url")
+    is_display_tracker, tracker = display_tracker_options()
+    if st.sidebar.button('Detect Objects'):
+        try:
+            vid_cap = cv2.VideoCapture(source_rtsp)
+            st_frame = st.empty()
+            while (vid_cap.isOpened()):
+                success, image = vid_cap.read()
+                if success:
+                    _display_detected_frames(conf,
+                                             model,
+                                             st_frame,
+                                             image,
+                                             is_display_tracker,
+                                             tracker
+                                             )
+                else:
+                    vid_cap.release()
+                    break
+        except Exception as e:
+            st.sidebar.error("Error loading RTSP stream: " + str(e))
+
+
+
+
+def play_webcam(conf, model):
+    """
+    Plays a webcam stream. Detects Objects in real-time using the YOLO object detection model.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    st.sidebar.title("Webcam Object Detection")
+
+    webrtc_streamer(
+        key="example",
+        mode = WebRtcMode.SENDRECV,
+        rtc_configuration={"iceServers": get_ice_servers()},
+        # video_frame_callback=callback,
+        video_transformer_factory=lambda: MyVideoTransformer(conf, model),
+        # video_processor_factory=lambda : utils.MyVideoTransformer(conf,model),        
+        media_stream_constraints={"video": True, "audio": False},
+        async_processing =True
+
+
+        
+     #   video_processor_factory=lambda: MyVideoTransformer(conf, model),
+     #   rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+     #   media_stream_constraints={"video": True, "audio": False},
+    )
+
+class MyVideoTransformer(VideoTransformerBase):
+    def __init__(self, conf, model):
+        self.conf = conf
+        self.model = model
+
+    def recv(self, frame):
+        image = frame.to_ndarray(format="bgr24")
+        processed_image = self._display_detected_frames(image)
+        st.image(processed_image, caption='Detected Video', channels="BGR", use_column_width=True)
+        return av.VideoFrame.from_ndarray(processed_image, format="bgr24")
+
+    def _display_detected_frames(self, image):
+        orig_h, orig_w = image.shape[0:2]
+        width = 720  # Set the desired width for processing
+
+        # cv2.resize used in a forked thread may cause memory leaks
+        input = np.asarray(Image.fromarray(image).resize((width, int(width * orig_h / orig_w))))
+
+        if self.model is not None:
+            # Perform object detection using YOLO model
+            res = self.model.predict(input, conf=self.conf)
+
+            # Plot the detected objects on the video frame
+            res_plotted = res[0].plot()
+            return res_plotted
+
+        return input
+
+
+
+def play_stored_video(conf, model):
+    """
+    Plays a stored video file. Tracks and detects objects in real-time using the YOLOv8 object detection model.
+
+    Parameters:
+        conf: Confidence of YOLOv8 model.
+        model: An instance of the `YOLOv8` class containing the YOLOv8 model.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    source_vid = st.sidebar.selectbox(
+        "Choose a video...", settings.VIDEOS_DICT.keys())
+
+    is_display_tracker, tracker = display_tracker_options()
+
+    with open(settings.VIDEOS_DICT.get(source_vid), 'rb') as video_file:
+        video_bytes = video_file.read()
+    if video_bytes:
+        st.video(video_bytes)
+
+    if st.sidebar.button('Detect Video Objects'):
+        try:
+            vid_cap = cv2.VideoCapture(
+                str(settings.VIDEOS_DICT.get(source_vid)))
+            st_frame = st.empty()
+            while (vid_cap.isOpened()):
+                success, image = vid_cap.read()
+                if success:
+                    _display_detected_frames(conf,
+                                             model,
+                                             st_frame,
+                                             image,
+                                             is_display_tracker,
+                                             tracker
+                                             )
+                else:
+                    vid_cap.release()
+                    break
+        except Exception as e:
+            st.sidebar.error("Error loading video: " + str(e))
