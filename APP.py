08e238748db7d74b2c351f153d91ecfdb154e870
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-12-13 09:19:16 -0600

    Update APP.py

--- APP.py
+++ APP.py
@@ -38,7 +38,14 @@ except Exception as ex:
     st.error(f"Unable to load model. Check the specified path: {model_path}")
     st.error(ex)
 
+st.sidebar.header("Imagen/Config")
+source_radio = st.sidebar.radio(
+    "Seleccione Fuente", settings.SOURCES_LIST)
+
+
     source_img = None
+    # If image is selected
+    if source_radio == settings.IMAGE:
     source_img = st.sidebar.file_uploader(
         "Elige una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
 
