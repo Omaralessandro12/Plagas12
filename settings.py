01b9eb3d9403cb99c7be6c4d884accd959d5f5e2
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-12-13 09:01:31 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -29,7 +29,7 @@ DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
 
 
 # ML Model config
-MODEL_DIR = ROOT / 'carpeta'
+MODEL_DIR = ROOT / 'CARPETA'
 DETECTION_MODEL = MODEL_DIR / 'best.pt'
 SEGMENTATION_MODEL = MODEL_DIR / 'yolov8n-seg.pt'
 
